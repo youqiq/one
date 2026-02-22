bb5d7e14674c43e22238b14dcee8c4314231a699
youqiq <shsbshwbs3@gmail.com>
2026-02-23 05:41:25 +0700

    Update build.gradle.kts

--- AnimeSail/build.gradle.kts
+++ AnimeSail/build.gradle.kts
@@ -7,7 +7,7 @@ cloudstream {
     // All of these properties are optional, you can safely remove them
 
     // description = "Lorem Ipsum"
-    authors = listOf("Mohirufy")
+    authors = listOf("youqiq")
 
     /**
      * Status int as the following:
