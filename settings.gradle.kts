c8d747cefd0aa5e38289ad40b0fffaac6952df30
youqiq <shsbshwbs3@gmail.com>
2026-02-23 05:34:31 +0700

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-rootProject.name = "CloudstreamPlugins"
+rootProject.name = "BilibiliTV"
 
 // This file sets what projects are included. All new projects should get automatically included unless specified in "disabled" variable.
 val disabled = listOf<String>()
