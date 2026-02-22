1eff8a57babf44079ae45e84efde42ce224c4d7b
youqiq <shsbshwbs3@gmail.com>
2026-02-23 05:33:21 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 }
 
 dependencies {
-implementation("com.github.recloudstream:cloudstream:master-SNAPSHOT")
+    implementation("com.github.recloudstream:cloudstream:master-SNAPSHOT")
 }
 
 tasks.jar {
